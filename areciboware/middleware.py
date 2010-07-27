bc053a7898b116ad7e4728b006bde05f22b31d22
wichert <wichert@1e5bcb8d-56ff-0310-93c8-940903db77ed>
2010-07-27 07:38:05 +0000

    Add request data with all  CGI and WSGI variables.
    git-svn-id: http://code.simplon.biz/svn/areciboware/trunk@14660 1e5bcb8d-56ff-0310-93c8-940903db77ed

--- areciboware/middleware.py
+++ areciboware/middleware.py
@@ -1,9 +1,10 @@
 import re
 import logging
 import traceback
-from paste import request
+import textwrap
 import arecibo
 from weberror.errormiddleware import ErrorMiddleware
+from weberror.errormiddleware import Supplement
 
 log = logging.getLogger(__name__)
 
@@ -41,17 +42,48 @@ class AreciboMiddleware(ErrorMiddleware):
             self.account = global_conf.get("account")
 
 
+    def addRequestInfo(self, report, supplement):
+        extra=supplement.extraData()
+        output=[]
+
+        for (title, data) in sorted(extra.items()):
+            if title[0]!="extra":
+                continue
+            title=title[1]
+            output.append(title)
+            output.append("="*len(title))
+            for (key, value) in sorted(data.items()):
+                wrapper=textwrap.TextWrapper(
+                        width=100,
+                        break_long_words=False,
+                        break_on_hyphens=False,
+                        drop_whitespace=False,
+                        initial_indent="%-25s" % key,
+                        subsequent_indent=" "*25)
+                if not isinstance(value, basestring):
+                    value=repr(value)
+                output.extend(wrapper.wrap(value))
+            output.append("")
+            output.append("")
+
+        if output:
+            report.set("request", "\n".join(output))
+
+
+
     def report(self, exc_info, environ):
+        supplement = Supplement(self, environ)
         report = arecibo.post()
         report.server(url=self.url)
         report.set("account", self.account)
         report.set("status", "500")
-        report.set("url", request.construct_url(environ))
+        report.set("url", supplement.source_url)
         report.set("user_agent", environ.get("HTTP_USER_AGENT"))
         report.set("ip", remote_address(environ))
         report.set("type", exc_info[0].__name__)
         if "REMOTE_USER" in environ:
             report.set("username", environ["REMOTE_USER"])
+        self.addRequestInfo(report, supplement)
         report.set("traceback", traceback.format_exc(exc_info[2]))
         report.send()
 
